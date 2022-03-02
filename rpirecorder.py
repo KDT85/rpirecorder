d33afa4b3dee39575b336e531bc8856cc391513c
K D T <62796155+KDT85@users.noreply.github.com>
2022-03-02 10:30:47 +0000

    rpirecorder.py
    python code for recording audio

---
+++ rpirecorder.py
@@ -0,0 +1,30 @@
+import subprocess
+from time import sleep
+#import signal
+from datetime import datetime
+from threading import Thread
+
+date = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+
+cmd = [
+    'arecord',
+    '-f',
+    'cd',
+    date + '.mp3'
+    ]
+
+
+def rec_start():
+    print("recording")
+    subprocess.run(cmd)#-D hw:2,0 -d 5 -f cd test.wav -c 1
+    
+    
+
+def rec_stop():
+    subprocess.run(['pkill','arecord'])
+#p.send_signal(signal.SIGINT)
+    print('recording finished')
+
+Thread(target=rec_start).start()
+sleep(10)
+rec_stop()
